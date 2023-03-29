38b73c029f85fd2eacfa13c5074de0cb4b5a4994
parvesh-piccosoft <119719731+parvesh-piccosoft@users.noreply.github.com>
2023-03-29 10:48:09 +0530

    changes

---
+++ chat.go
@@ -0,0 +1,88 @@
+package gochat
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func AddRoute(r *gin.RouterGroup) *gin.RouterGroup {
+
+	r.GET("/", Index)
+	r.GET("/ws", HandleConnections)
+
+	return r
+}
+
+var clients = make(map[*websocket.Conn]bool) // connected clients
+
+var broadcast = make(chan Message) // broadcast channel
+
+// Configure the upgrader
+var upgrader = websocket.Upgrader{
+
+	CheckOrigin: func(r *http.Request) bool {
+
+		return true
+	},
+}
+
+func Index(c *gin.Context) {
+
+	c.HTML(200, "index.html", nil)
+}
+
+func HandleConnections(c *gin.Context) {
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
+
+	// Register our new client
+	clients[ws] = true
+
+	for {
+		var msg Message
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println(err)
+			delete(clients, ws)
+			break
+		}
+		// Send the newly received message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func HandleMessages() {
+	for {
+		// Grab the next message from the broadcast channel
+		msg := <-broadcast
+		// Send it out to every client that is currently connected
+		for client := range clients {
+
+			err := client.WriteJSON(msg)
+
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+
+}
--- models/message.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Message struct {
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Message  string `json:"message"`
-}
